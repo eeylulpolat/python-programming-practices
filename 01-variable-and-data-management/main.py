ef53ba1d4abd6d0432b7bb0a601c263d38db5028
Eylül Polat <eeylulpolat@hotmail.com>
2026-06-25 23:06:39 +0300

    Created a band name generator and practiced terms input and variables.

---
+++ 01-variable-and-data-management/main.py
@@ -0,0 +1,5 @@
+#Band Name Generator
+print("Hello! Welcome to the Band Name Generator.")
+city = input("What city did you grow up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name can be " + city + " " + pet_name + ".")
